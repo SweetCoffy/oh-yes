779821f9ce034e02087b0c40f104ac6fbc937697
Sebo2205 <sebasaleru3@gmail.com>
2022-05-26 17:08:06 -0400

    remove useless thing in hot-reloadable.ts

--- src/hot-reloadable.ts
+++ src/hot-reloadable.ts
@@ -2,7 +2,6 @@ import commands from "./hot-reloadable/commands.js";
 import economy from "./hot-reloadable/economy.js";
 
 export default {
-    sus: "no",
     commands: {} as typeof commands,
     eco: {} as typeof economy,
     loadfiles: [{ file: "hot-reloadable/commands.js", name: "commands" }, { file: "hot-reloadable/economy.js", name: "eco" }]
