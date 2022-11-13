2b39734902432677837b2352585e74db4d252f93
Sebowo2205 <sebowo2205@gmail.com>
2022-11-12 21:00:53 -0400

    actually implement give command

--- src/commands/dev/give.ts
+++ src/commands/dev/give.ts
@@ -1,7 +1,14 @@
 import { Command } from "../../types";
+import { addItem, getUser, itemString } from "../../util.js";
 
 export default {
     name: "give",
-    description: "Gives an item to a user, with no validation applied.",
+    description: "Gives an item to you, with no validation applied.",
     devOnly: true,
+    args: [{ name: "item", type: "string", required: true }, { name: "amount", type: "bigint", required: false }],
+    async run(msg, item: string, amount: bigint = 1n) {
+        let u = await getUser(msg.author)
+        addItem(u, item, amount)
+        await msg.reply(`${itemString(item, amount)} given.`)
+    },
 } as Command
