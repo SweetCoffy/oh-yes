59fe1dc31a306cd6f4149ef34e77f39ff3d62363
Sebowo2205 <sebowo2205@gmail.com>
2022-11-20 17:36:18 -0400

    remove unused item uses

--- src/hot-reloadable/yaml-funcs.ts
+++ src/hot-reloadable/yaml-funcs.ts
@@ -14,15 +14,6 @@ export default {
                 addMul(u, i, total);
             }
         },
-        example_item: (u: UserData, a: bigint) => u.money.points = 0n,
-        cookie: (u: UserData, a: bigint) => void addMul(u, 0, 1n * a / 50n),
-        bread: (u: UserData, a: bigint) => void addMul(u, 0, 1n * a / 5n),
-        baguette: (u: UserData, a: bigint) => void addMul(u, 0, 1n * a / 2n - (a / 10n)),
-        spaghet: (u: UserData, a: bigint) => void addMul(u, 0, 1n * a),
-        moon_cake: (u: UserData, a: bigint) => void addMul(u, 0, 2n * a + (a / 4n)),
-        avocado: (u: UserData, a: bigint) => void addMul(u, 0, a * 7n + (a / 4n)),
-        egg: (u: UserData, a: bigint) => void addMul(u, 0, 500_000n),
-        milk: (u: UserData, a: bigint) => void addMul(u, 1, 1n * a),
         venezuela_flag: (u: UserData, a: bigint) => {
             if (u.progression < Progression.VenezuelaMode) u.progression = Progression.VenezuelaMode
             u.vzMode = true
