f07b1cbefa416c1a9f961f1f4ee27c437967cf72
SweetCoffy <sebowo2205@gmail.com>
2022-12-02 23:45:24 -0400

    add uptime to ;bot-info

--- src/commands/dev/bot-info.ts
+++ src/commands/dev/bot-info.ts
@@ -2,14 +2,18 @@ import { Command } from "../../types.js";
 import { release, totalmem, type } from "os"
 import { EmbedBuilder } from "discord.js";
 import { sizeFormat } from "../../formats.js";
-import { formatNumber } from "../../util/formatting.js";
+import { formatNumber, formatTime } from "../../util/formatting.js";
 
 export default {
     name: "bot-info",
     async run(msg) {
         let embed = new EmbedBuilder()
             .setTitle(`Bot Info`)
-            .setDescription(`**OS**: ${type()} ${release()}\n**Env**: ${process.env.NODE_ENV ?? '???'}\n**Mem**: ${formatNumber(totalmem(), sizeFormat)}\n**Node Ver**: ${process.version}`)
+            .setDescription(
+                `**OS**: ${type()} ${release()}\n` +
+                `**Mem**: ${formatNumber(totalmem(), sizeFormat)}\n` +
+                `**Node Ver**: ${process.version}\n` +
+                `**Uptime**: ${formatTime((msg.client.uptime ?? 0) / 1000)}`)
         await msg.reply({ embeds: [embed] })
     },
 } as Command
